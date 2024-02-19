c5a967a83e229526cae166e816fc07c039b506ee
Pautog11 <christiancosio04@gmail.com>
2024-02-19 08:40:41 +0800

    2/19/2024

--- SalesMonitoringSystem/Custom/Pos.xaml.vb
+++ SalesMonitoringSystem/Custom/Pos.xaml.vb
@@ -1,18 +1,19 @@
-﻿Imports System.Collections.ObjectModel
-Imports System.Data
-Imports HandyControl.Controls
-Imports HandyControl.Tools.Extension
-
+﻿
 Public Class Pos
+
+    'Private _dataTable As New sgsmsdb.viewtblproductsDataTable
     Public Sub New()
 
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+        'dim n As Integer = 10
         For i = 0 To 100
             Wrappanelxd.Children.Add(New ProductCard())
         Next
+
+
     End Sub
 
 End Class
--- SalesMonitoringSystem/Custom/ProductCard.xaml.vb
+++ SalesMonitoringSystem/Custom/ProductCard.xaml.vb
@@ -1,11 +1,3 @@
-﻿Public Class ProductCard
-
-    Public Sub New()
-
-        ' This call is required by the designer.
-        InitializeComponent()
-
-        ' Add any initialization after the InitializeComponent() call.
-    End Sub
+﻿Public Class ProductCardq
 
 End Class
